db5620a5fd288c5e51f3827984298e66587095d6
ryota0624 <tennis.suta6@gmail.com>
2022-05-26 16:07:41 +0900

    minitestが実行できるようにrubyのバージョンを変更

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "body length should be >= 10" do
+    article = Article.new(body: "")
+    assert_not article.save
+    assert_not_empty article.errors
+  end
 end
